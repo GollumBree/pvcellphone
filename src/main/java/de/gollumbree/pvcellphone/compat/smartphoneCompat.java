097b7998bbb0aa02468e383b2c960beb3a0de48a
GollumBree <haydn3lu.da@gmail.com>
2025-07-27 13:30:52 +0200

    1.0.2, used new smartphone compat

--- src/main/java/de/gollumbree/pvcellphone/compat/smartphoneCompat.java
+++ src/main/java/de/gollumbree/pvcellphone/compat/smartphoneCompat.java
@@ -1,30 +1,17 @@
 package de.gollumbree.pvcellphone.compat;
 
-import java.util.stream.Stream;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import de.gollumbree.pvcellphone.items.CellphoneItem;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.ItemContainerContents;
-import de.gollumbree.smartphone.items.SmartphoneItem;
+import de.gollumbree.smartphone.compat.ItemFinder;
 
 public class smartphoneCompat {
     public static @Nullable CellphoneItem callSmartphone(@Nonnull Inventory inv) {
-        Stream<ItemStack> smartphoneContents = inv.items.stream() // Stream all items in the inventory
-                .filter(itemStack -> itemStack.getItem() instanceof SmartphoneItem) // Filter Smartphones
-                .map(item -> item.get(DataComponents.CONTAINER)) // get their contents
-                .filter(contents -> contents != null).flatMap(ItemContainerContents::stream); // Discard null contents
-                                                                                              // and flatten the stream
-
-        return smartphoneContents
-                .map(ItemStack::getItem)
-                .filter(item -> item instanceof CellphoneItem) // Filter CellphoneItems from the smartphone contents
-                .map(item -> (CellphoneItem) item) // Cast to CellphoneItem
-                .findFirst() // Get the first CellphoneItem found
-                .orElse(null); // Return null if no CellphoneItem is found
+        @Nullable
+        ItemStack cellphoneStack = ItemFinder.findItem(inv, CellphoneItem.class);
+        return cellphoneStack != null && cellphoneStack.getItem() instanceof CellphoneItem cellphone ? cellphone : null;
     }
 }
